0ce28b3206a036b00e32af545f81b6d260a2e849
jens lindner <jens.lindner@camunda.com>
2025-07-21 18:58:31 +0200

    initial commit after creation

---
+++ src/test/java/org/camunda/consulting/stater/jenstasklist/JensTasklistApplicationTests.java
@@ -0,0 +1,13 @@
+package org.camunda.consulting.stater.jenstasklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JensTasklistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
