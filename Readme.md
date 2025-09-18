18a9e62cec1aa24ea1247dca706753c5ae3da096
jens lindner <jens.lindner@camunda.com>
2025-09-18 20:13:41 +0200

    Add configs for self-managed and c8run

--- Readme.md
+++ Readme.md
@@ -3,3 +3,20 @@
 This project contains a very small and not very good looking example of an Angular Tasklist for C8.
 This is the most Frontend Code I've written in the last decade, so everything is pretty clumsy, but APIs can be called and UIs rendered based on formKeys :-)
 I hope to make this more sophisticated in the near future, if time permits.
+
+
+## Retrieve Cookie from C8Run
+```bash
+curl --request POST 'http://localhost:8080/api/login?username=demo&password=demo' \
+--cookie-jar cookie.txt
+```
+
+## Query Operate via facade
+```bash
+curl localhost:8081/api/processes
+```
+
+## Query Tasklist via facade
+```bash
+curl localhost:8081/api/tasks
+```
--- src/main/java/org/camunda/consulting/jenstasklist/JensTasklistApplication.java
+++ src/main/java/org/camunda/consulting/jenstasklist/JensTasklistApplication.java
@@ -1,40 +1,60 @@
 package org.camunda.consulting.jenstasklist;
 
 import io.camunda.zeebe.client.ZeebeClient;
+import io.camunda.zeebe.client.ZeebeClientBuilder;
+import io.camunda.zeebe.client.impl.ZeebeClientBuilderImpl;
 import io.camunda.zeebe.spring.client.annotation.Deployment;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Component;
 
+import java.net.URI;
 import java.util.Map;
 
 @SpringBootApplication
-@Deployment(resources = "classpath*:/models/*.bpmn")
+//@Deployment(resources = "classpath*:/models/*.bpmn")
 public class JensTasklistApplication {
-
-    @Autowired
-    private ZeebeClient zeebeClient;
 
     public static void main(String[] args) {
         SpringApplication.run(JensTasklistApplication.class, args);
     }
 
     @Component
-    public class Initializer implements CommandLineRunner {
+    @Slf4j
+    public static class Initializer implements CommandLineRunner {
 
         @Override
         public void run(String... args) throws Exception {
-            zeebeClient.newCreateInstanceCommand().bpmnProcessId("Process1").latestVersion()
+            ZeebeClientBuilder zeebeClientBuilder = new ZeebeClientBuilderImpl();
+            ZeebeClient zeebeClient = zeebeClientBuilder
+                    .restAddress(URI.create("http://localhost:8080"))
+                    .grpcAddress(URI.create("http://localhost:26500"))
+                    .usePlaintext()
+                    .preferRestOverGrpc(false)
+                    .withChainHandlers(
+                            (request, producer, scope, chain, callback) -> {
+                                request.setHeader("Cookie", "OPERATE-SESSION=F614998185D7F90B7204A97EB139927A");
+                                chain.proceed(request, producer, scope, callback);
+                            }).build();
+
+            log.info(".... Zeebe Client built.");
+            log.info(".... now creating instances");
+
+            zeebeClient.
+                    newCreateInstanceCommand().bpmnProcessId("Process1").latestVersion()
                     .variables(Map.of("assignee1", "manu", "assignee2", "jens", "creationTimestamp", System.currentTimeMillis()))
                     .send()
                     .join();
+            log.info(".... instance 1 created!");
 
             zeebeClient.newCreateInstanceCommand().bpmnProcessId("Process2").latestVersion()
                     .variables(Map.of("assignee1", "jens", "assignee2", "jens", "creationTimestamp", System.currentTimeMillis()))
                     .send()
                     .join();
+            log.info(".... instance 2 created!");
         }
 
     }
